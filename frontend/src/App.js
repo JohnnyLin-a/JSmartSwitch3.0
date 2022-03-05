17eee3bae83d7ad7e31212b6bc1b9e2f46049440
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-03-05 17:29:10 -0500

    Add dirty code to "make it work"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,35 @@
 
 function App() {
+
+  const openComputer = async () => {
+    console.log("openComputer")
+    fetch('/api/v1/wol/wake', { method: 'POST'})
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  const openLights = async () => {
+    console.log("openLights")
+    fetch('/api/v1/lights/on', { method: 'POST'})
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
+  const closeLights = async () => {
+    console.log("closeLights")
+    fetch('/api/v1/lights/off', { method: 'POST'})
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }
+
   return (
-    <div>
+    <div style={{ backgroundColor: "black"}}>
       <header>
-        <p>Barebones edition</p>
+        <p>Year 2000 edition</p>
       </header>
+      <button onClick={openComputer}>Power on computer</button><br/><br/>
+      <button onClick={openLights}>Open lights</button><br/><br/>
+      <button onClick={closeLights}>Close lights</button>
     </div>
   );
 }
