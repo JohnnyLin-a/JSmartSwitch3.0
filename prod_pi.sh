1a4dbca0027f2956ff7225ca3c448790f2f68de5
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-03-05 19:43:42 -0500

    Add rpi optimized build script

---
+++ prod_pi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# A raspberry pi device is very weak, and needs to be built on external machines. Otherwise it takes over 1000seconds...
+# Setup buildx and point the builder to the external server
+# Then run this script.
+# buildx can be set up this way: docker buildx create --name foo --platform linux/arm/v7 ssh://johnnylin-a@bar
+docker buildx build --load -t jsmartswitch30_frontend -f Dockerfile.node .
+docker buildx build --load -t jsmartswitch30_backend -f Dockerfile.golang .
+docker buildx build --load -t jsmartswitch30_nginx -f Dockerfile.nginx .
+docker compose -f docker-compose-pi.yml up -d
