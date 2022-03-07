e6feb909f239b356498b17d1dce35c95399373bc
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-03-07 01:09:39 -0500

    Add status message

--- frontend/src/components/pages/SmartSwitch/SmartSwitch.js
+++ frontend/src/components/pages/SmartSwitch/SmartSwitch.js
@@ -4,6 +4,7 @@ import "./SmartSwitch.css"
 
 const SmartSwitch = () => {
   const [confirmations, setConfirmations] = useState({});
+  const [message, setMessage] = useState("");
 
   const resetConfirmation = key => {
     return () => {
@@ -38,14 +39,26 @@ const SmartSwitch = () => {
 
   const openComputer = async () => {
     fetch('/api/v1/wol/wake', { method: 'POST'})
+    setMessage("Opened computer")
+    setTimeout(() => {
+      setMessage("")
+    }, 3000)
   }
 
   const openLights = async () => {
     fetch('/api/v1/lights/on', { method: 'POST'})
+    setMessage("Opened lights")
+    setTimeout(() => {
+      setMessage("")
+    }, 3000)
   }
 
   const closeLights = async () => {
     fetch('/api/v1/lights/off', { method: 'POST'})
+    setMessage("Closed lights")
+    setTimeout(() => {
+      setMessage("")
+    }, 3000)
   }
 
   return (
@@ -53,7 +66,7 @@ const SmartSwitch = () => {
       <div className="super-center" style={{ height: "25%" }}>
         <h1 style={{ margin: 0, textAlign: "center", color: "white", fontSize: "8em" }}>Command Center</h1>
       </div>
-      <div className="super-center flex-column" style={{ height: "75%" }}>
+      <div className="super-center flex-column" style={{ height: "55%" }}>
         <div className="m-3" style={{ width: "80%", height: "8rem" }}>
           <button className="btn btn-warning btn-block" style={{ width: "100%", height: "100%", fontSize: "3em" }} onClick={misclickConfirm(openComputer, "Open computer")}>{typeof confirmations["Open computer"] === 'undefined' ? "Open computer" : "Confirm?"}</button>
         </div>
@@ -65,6 +78,9 @@ const SmartSwitch = () => {
             <button className="btn btn-danger btn-block super-max" style={{ fontSize: "3em" }} onClick={closeLights}>Close lights</button>
           </div>
         </div>
+      </div>
+      <div style={{ textAlign: "center", color: "white", fontSize: "3em" }}>
+        {message}
       </div>
     </div>
   )
