a48e36a84228ddf75be2c37e0fc479eef32d033f
JohnnyLin-a <57846020+JohnnyLin-a@users.noreply.github.com>
2022-03-08 22:44:18 -0500

    Add scan function to scan and cache devices

--- internal/lights/lights.go
+++ internal/lights/lights.go
@@ -1,7 +1,6 @@
 package lights
 
 import (
-	"log"
 	"time"
 
 	"github.com/JohnnyLin-a/JSmartSwitch3.0/internal/config"
@@ -10,15 +9,44 @@ import (
 	"github.com/johnnylin-a/go-magic-home/pkg/magichome"
 )
 
-func PowerMagicHome(powerState bool) error {
+var (
+	magicHomeDevices *[]magichome.Device
+	hs100Devices     []*hs100.Hs100
+)
+
+func init() {
+	hs100Devices = make([]*hs100.Hs100, 0)
+	go Scan()
+}
+
+func Scan() {
+	ScanMagicHome()
+	ScanHS100()
+}
+
+func ScanMagicHome() {
 	devices, err := magichome.Discover(magichome.DiscoverOptions{
 		BroadcastAddr: config.Get.MagicHome.BroadcastIP,
 		Timeout:       1,
 	})
 	if err != nil {
-		return err
+		magicHomeDevices = devices
 	}
-	for _, device := range *devices {
+}
+
+func ScanHS100() {
+	devices, err := hs100.Discover(config.Get.HS100.BroadcastIP, configuration.Default().WithTimeout(time.Second))
+	if err != nil {
+		hs100Devices = devices
+	}
+}
+
+func PowerMagicHome(powerState bool) error {
+	if magicHomeDevices == nil || len(*magicHomeDevices) == 0 {
+		return nil
+	}
+
+	for _, device := range *magicHomeDevices {
 		controller, err := magichome.New(device.IP, 5577)
 		if powerState && err == nil {
 			controller.SetState(magichome.On)
@@ -30,12 +58,11 @@ func PowerMagicHome(powerState bool) error {
 }
 
 func PowerHS100(powerState bool) error {
-	devices, err := hs100.Discover(config.Get.HS100.BroadcastIP, configuration.Default().WithTimeout(time.Second))
-	if err != nil {
-		return err
+	if len(hs100Devices) == 0 {
+		return nil
 	}
-	log.Println(devices)
-	for _, device := range devices {
+
+	for _, device := range hs100Devices {
 		if powerState {
 			device.TurnOn()
 		} else {
